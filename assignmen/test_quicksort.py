a1e55cd0279048c1d7167a977dccf6d1750529a1
Mahesamyu <maheprakash87@gmail.com>
2025-07-28 23:03:13 +0530

    Update test_quicksort.py

--- assignmen/test_quicksort.py
+++ assignmen/test_quicksort.py
@@ -1,9 +1,9 @@
 import unittest
-from assignment.quicksort import quicksort
+import sys
+sys.path.insert(0, './assignmen')  # Change to 'assignment' if renamed
+from quicksort import quicksort
 
 class TestQuickSort(unittest.TestCase):
     def test_basic(self):
         self.assertEqual(quicksort([3, 1, 2]), [1, 2, 3])
 
-if __name__ == '__main__':
-    unittest.main()
